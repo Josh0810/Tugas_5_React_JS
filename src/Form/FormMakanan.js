89198094b8b5acc9e969df8e6ea3e2444f4f16ed
Josh0810 <92926113+Josh0810@users.noreply.github.com>
2022-02-02 23:39:25 +0700

    Commit

---
+++ src/Form/FormMakanan.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+class FormMakanan extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { pesanan: "", jumlah: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeJumlah = this.handleChangeJumlah.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.pesanan = React.createRef();
+  }
+
+  handleChange(e) {
+    this.setState({ pesanan: e.target.value });
+  }
+  handleChangeJumlah(e) {
+    this.setState({ jumlah: e.target.value });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    alert(
+      "Pesanan Anda Adalah : " +
+        this.state.pesanan +
+        " | Jumlah Pesanan : " +
+        this.state.jumlah
+    );
+    this.setState({ pesanan: "", jumlah: "" });
+    this.pesanan.current.focus();
+  }
+
+  render() {
+    return (
+      <div>
+        <center>
+          <form>
+            <input
+              type={"text"}
+              onChange={this.handleChange}
+              value={this.state.pesanan}
+              ref={this.pesanan}
+              placeholder="Masukan Pesanan Anda"
+            />
+            <input
+              type={"number"}
+              value={this.state.jumlah}
+              onChange={this.handleChangeJumlah}
+              placeholder="Masukan Jumlah Pesanan"
+            />
+            <br />
+            <br />
+            <input type={"submit"} onClick={this.handleSubmit} value={"Pesan"} />
+          </form>
+        </center>
+      </div>
+    );
+  }
+}
+export default FormMakanan;
